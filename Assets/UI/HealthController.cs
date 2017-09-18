865a590535c2548709dba66cab5ff634f75481b8
Stanislav Silin <byme.work@gmail.com>
2017-09-19 00:13:58 +0300

    Add text with health

---
+++ Assets/UI/HealthController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using GameSaving;
+using GameSaving.States;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+using GameSaving.MonoBehaviours;
+
+public class HealthController : MonoBehaviour
+{
+    public Main Main;
+    public Text Text;
+    private Lizard character;
+
+    void Start()
+    {
+        this.Main.GameController.Loaded.Subscribe(_ =>
+        {
+            this.character = EntitiesStorage.Instance.Entities.Values.OfType<Entity>().Where(o => o.GetComponent<Lizard>() != null).First().GetComponent<Lizard>();
+        });
+    }
+
+    void Update()
+    {
+        if (this.character)
+        {
+            this.Text.text = this.character.Health.ToString();
+        }
+    }
+}
