847ddbcd20d033d88efbc7fcd89cb0c9048a8ab9
Stanislav Silin <silin.stanislav@gamil.com>
2017-10-28 21:49:37 +0300

    Rework level manger to await/async

--- Assets/Code/GameObjects/Levels/LevelManager.cs
+++ Assets/Code/GameObjects/Levels/LevelManager.cs
@@ -11,25 +11,11 @@ public class LevelManager
 
     public async Task Load(LevelInformation level)
     {
-        // TODO: Think about how to refactor it
-        var loaded = false;
-
-        Observable.FromCoroutine(() => this.LoadInternal(level)).
-            Subscribe(_ => loaded = true);
-
-        while (!loaded)
-        {
-            await Task.Delay(100);
-        }
-    }
-
-    private IEnumerator LoadInternal(LevelInformation level)
-    {
         if (this.CurrentLevel != null)
-            yield return SceneManager.UnloadSceneAsync(this.CurrentLevel.Scene);
+            await SceneManager.UnloadSceneAsync(this.CurrentLevel.Scene);
 
         this.CurrentLevel = level;
-        yield return SceneManager.LoadSceneAsync(level.Scene, LoadSceneMode.Additive);
+        await SceneManager.LoadSceneAsync(level.Scene, LoadSceneMode.Additive);
         var scene = SceneManager.GetSceneByName(level.Scene);
         SceneManager.SetActiveScene(scene);
     }
