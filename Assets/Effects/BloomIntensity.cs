d849c2638b1c83245a210a2b4a52b7812f355483
Stanislav Silin <silin.stanislav@outlook.com>
2018-08-17 12:47:02 +0300

    Added changing the bloom intensity for capsule

---
+++ Assets/Effects/BloomIntensity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Effects
+{
+	[RequireComponent(typeof(SpriteRenderer))]
+	public class BloomIntensity : MonoBehaviour
+	{
+		public float deltaIntesity;
+		public float speed;
+		private SpriteRenderer spriteRender;
+		private float initialIntensity;
+
+		private void Start()
+		{
+			this.spriteRender = this.GetComponent<SpriteRenderer>();
+			this.initialIntensity = this.spriteRender.material.GetColor("_EmissionColor").r;
+		}
+
+		private void Update()
+		{
+			var currentIntensity = this.initialIntensity + Mathf.Sin(Time.deltaTime) / this.speed * this.deltaIntesity;
+			var color = new Color(currentIntensity, currentIntensity, currentIntensity);
+			this.spriteRender.material.SetColor("_EmissionColor", color);
+		}
+	}
+}
