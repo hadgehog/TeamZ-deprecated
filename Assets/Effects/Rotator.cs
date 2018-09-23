9a677f5c8c0b167dee581181b7cb55bf93a9d8a6
Stanislav Silin <silin.stanislav@outlook.com>
2018-09-23 15:13:59 +0300

    Added rotation to red lamp

---
+++ Assets/Effects/Rotator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+	public float rotationSpeed = 1;
+	public bool useRandomBoost = true;
+	public float randomBoostValue = 1;
+	public int randomBoostSeed = 0;
+
+	private Vector3 xAxis;
+	private Transform currentTransform;
+	private System.Random random;
+	private Action update;
+
+	private void Start()
+	{
+		this.xAxis = new Vector3(1, 0, 0);
+		this.currentTransform = this.transform;
+
+		if (this.useRandomBoost)
+		{
+			this.update = () => this.currentTransform.Rotate(this.xAxis, Math.Max(this.rotationSpeed, this.rotationSpeed + Mathf.Sin(Time.time) * this.randomBoostValue));
+		}
+		else
+		{
+			this.update = () => this.currentTransform.Rotate(this.xAxis, this.rotationSpeed);
+		}
+
+		this.random = new System.Random(this.randomBoostSeed);
+	}
+
+	private void Update()
+	{
+		this.update();
+	}
+}
