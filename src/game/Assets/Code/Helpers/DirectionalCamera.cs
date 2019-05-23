59a33ca0569e3fd336f4039d2d08819806105a52
Stanislav Silin <silin.stanislav@outlook.com>
2019-05-23 20:46:20 +0300

    Updated camera to support multipal characters

--- src/game/Assets/Code/Helpers/DirectionalCamera.cs
+++ src/game/Assets/Code/Helpers/DirectionalCamera.cs
@@ -8,33 +8,42 @@ using UnityEngine;
 
 public class DirectionalCamera : MonoBehaviour
 {
-	public float dampTime = 0.3f;
-	public Transform target;
+    public float dampTime = 0.3f;
 
-	private Vector3 velocity = Vector3.zero;
-	private Dependency<GameController> gameController;
-	private Dependency<EntitiesStorage> entitiesStorage;
+    private Vector3 velocity = Vector3.zero;
+    private Dependency<GameController> gameController;
+    private Dependency<EntitiesStorage> entitiesStorage;
+    private Transform[] targets;
 
-	private void Start()
-	{
-		this.gameController.Value.Loaded.Subscribe(_ => this.Loaded());
-	}
+    private void Start()
+    {
+        this.gameController.Value.Loaded.Subscribe(_ => this.Loaded());
+    }
 
-	private void Loaded()
-	{
-		this.target = this.entitiesStorage.Value.Entities.Values.
-			Where(o => o.GetComponent<Lizard>()).FirstOrDefault()?.transform;
-	}
+    private void Loaded()
+    {
+        this.targets = this.entitiesStorage.Value.Entities.Values.
+            Where(o => o.GetComponent<Lizard>()).Select(o => o.transform).ToArray();
+    }
 
-	private void Update()
-	{
-		if (this.target)
-		{
-			Vector3 point = this.GetComponent<Camera>().WorldToViewportPoint(new Vector3(this.target.position.x, this.target.position.y + 1.5f, this.target.position.z));
-			Vector3 delta = new Vector3(this.target.position.x, this.target.position.y + 1.5f, this.target.position.z) - this.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
-			Vector3 destination = this.transform.position + delta;
+    private void Update()
+    {
+        if (this.targets?.Any() ?? false)
+        {
+            var sum = Vector3.zero;
+            for (int i = 0; i < this.targets.Length; i++)
+            {
+                var target = this.targets[i];
+                var point = this.GetComponent<Camera>().WorldToViewportPoint(new Vector3(target.position.x, target.position.y + 1.5f, target.position.z));
+                var delta = new Vector3(target.position.x, target.position.y + 1.5f, target.position.z) - this.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+                var destination = this.transform.position + delta;
 
-			this.transform.position = Vector3.SmoothDamp(this.transform.position, destination, ref this.velocity, this.dampTime);
-		}
-	}
+                sum += destination;
+            }
+
+            var approximateDestination = sum / this.targets.Length;
+
+            this.transform.position = Vector3.SmoothDamp(this.transform.position, approximateDestination, ref this.velocity, this.dampTime);
+        }
+    }
 }
