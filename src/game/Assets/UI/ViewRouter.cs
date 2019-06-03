c2751956e035132e703a73ceed6182e198d72ca8
Vladislav <vladislav.neznauskas@gmail.com>
2019-06-03 17:32:56 +0300

    fixed tips that displayed in MainMenu

--- src/game/Assets/UI/ViewRouter.cs
+++ src/game/Assets/UI/ViewRouter.cs
@@ -1,41 +1,51 @@
 ﻿using Assets.Code.Helpers;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.UI
 {
-	public class ViewRouter : MonoBehaviour
-	{
-		public HUDController GameHUDView;
-		public MainView MainView;
-		public LoadView LoadView;
-		public SaveView SaveView;
-		public SettingsView SettingsView;
+    public class ViewRouter : MonoBehaviour
+    {
+        public HUDController GameHUDView;
+        public MainView MainView;
+        public LoadView LoadView;
+        public SaveView SaveView;
+        public SettingsView SettingsView;
 
         public UnityDependency<MainMenuBackgroundImage> BackgroundImage;
 
-		public void ShowGameHUDView()
-		{
-			this.DisableAll();
-			this.GameHUDView.Activate();
+        public void ShowGameHUDView()
+        {
+            this.DisableAll();
+            this.EnableTips();
+            this.GameHUDView.Activate();
             this.BackgroundImage.Value.Deactivate();
-		}
+        }
 
-		public void ShowMainView()
-		{
-			this.DisableAll();
+        public void ShowMainView()
+        {
+            this.DisableAll();
             this.MainView.Activate();
             this.BackgroundImage.Value.Activate();
         }
 
-        public void DisableAll()
-		{
-			foreach (Transform view in this.transform)
-			{
-				if (view.name != "Text")
-				{
-					view.gameObject.SetActive(false);
-				}
-			}
-		}
-	}
+        private void DisableAll()
+        {
+            foreach (Transform view in this.transform)
+            {
+                view.gameObject.SetActive(false);
+            }
+        }
+
+        private void EnableTips()
+        {
+            foreach (Transform view in this.transform)
+            {
+                if (view.name == "Text")
+                {
+                    view.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
 }
