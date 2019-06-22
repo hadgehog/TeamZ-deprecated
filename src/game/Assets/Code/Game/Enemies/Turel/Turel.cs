7ddba066d7dac38f5f3026c63468ba4d03c68641
Vladislav <vladislav.neznauskas@gmail.com>
2019-06-22 14:55:04 +0300

    Fixed enemy turret for shooting to both characters

--- src/game/Assets/Code/Game/Enemies/Turel/Turel.cs
+++ src/game/Assets/Code/Game/Enemies/Turel/Turel.cs
@@ -10,20 +10,56 @@ namespace TeamZ.Assets.Code.GameObjects.Enemies.Turel
 		public ParticleSystem BulletParticleSystem;
         public int Damage = 25;
 
-		UnityDependency<Lizard> lizard;
-		private readonly IDisposable shooting;
+        private UnityDependency<Lizard> lizard;
+        private UnityDependency<Hedgehog> hedgehog;
+
+        private readonly IDisposable shooting;
 
 		private void Start()
 		{
-			Observable.Interval(TimeSpan.FromSeconds(1)).
-				Where(o => this.lizard).
-				Subscribe(o =>
-				{
-					this.transform.LookAt(this.lizard);
-					this.BulletParticleSystem.Emit(1);
-				}).
-				AddTo(this);
-		}
+            if (this.lizard.Value != null && this.hedgehog.Value != null)
+            {
+                bool firstCharacterIsAim = true;
+
+                Observable.Interval(TimeSpan.FromSeconds(1)).
+                    Where(o => this.lizard || this.hedgehog).
+                    Subscribe(o =>
+                    {
+                        if (firstCharacterIsAim)
+                        {
+                            this.transform.LookAt(this.lizard);
+                        }
+                        else
+                        {
+                            this.transform.LookAt(this.hedgehog);
+                        }
+
+                        this.BulletParticleSystem.Emit(1);
+                        firstCharacterIsAim = !firstCharacterIsAim;
+                    }).
+                    AddTo(this);
+            }
+            else
+            {
+                Observable.Interval(TimeSpan.FromSeconds(1)).
+                    Where(o => this.lizard).
+                    Subscribe(o =>
+                    {
+                        this.transform.LookAt(this.lizard);
+                        this.BulletParticleSystem.Emit(1);
+                    }).
+                    AddTo(this);
+
+                Observable.Interval(TimeSpan.FromSeconds(1)).
+                    Where(o => this.hedgehog).
+                    Subscribe(o =>
+                    {
+                        this.transform.LookAt(this.hedgehog);
+                        this.BulletParticleSystem.Emit(1);
+                    }).
+                    AddTo(this);
+            }
+        }
 
         private void OnParticleCollision(GameObject other)
         {
